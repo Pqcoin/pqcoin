3573d22a0a2f073acc94ea7b1ee6706231733a44
PsychosisSky <38359502+PsychosisSky@users.noreply.github.com>
2024-04-21 14:47:46 +0800

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -90,7 +90,7 @@ public:
         consensus.BIP65Height = 3464751; // 34cd2cbba4ba366f47e5aa0db5f02c19eba2adf679ceb6653ac003bdc9a0ef1f - first v4 block after the last v3 block
         consensus.BIP66Height = 1034383; // 80d1364201e5df97e696c03bdd24dc885e8617b9de51e453c10a4f629b1e797a - this is the last block that could be v2, 1900 blocks past the last v2 block
         consensus.powLimit = uint256S("0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"); // ~uint256(0) >> 20;
-<<<<<<< HEAD
+
         consensus.initPowDifficulty = uint256S("0x000010ffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
 
         //wh_difficuly
@@ -98,12 +98,12 @@ public:
         // consensus.nPowTargetSpacing = 60; // 
         consensus.nPowTargetTimespan = 7*24*60*60;   // 2016块调整一次
         consensus.nPowTargetSpacing = 300;   // 期望300秒出一个块
-=======
+
         consensus.initPowDifficulty = uint256S("0x207fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"); 
         consensus.nPowTargetTimespan = 7*24*60*60;   
         consensus.nPowTargetSpacing = 300;   
 
->>>>>>> 14ee81c3d89ce99db8c58bef35983614ec0ed4ce
+
 
 
         consensus.fDigishieldDifficultyCalculation = false;
@@ -172,7 +172,7 @@ public:
         pchMessageStart[3] = 0xc4;
         nDefaultPort = 7332;
         nPruneAfterHeight = 100000;
-<<<<<<< HEAD
+
         genesis = CreateGenesisBlock(1681098301, 1, 0x207fffff, 1, 12000000 * COIN);
 //        std::cout<<genesis.GetHash().ToString()<<std::endl;
 
@@ -200,7 +200,7 @@ public:
         // Note that of those with the service bits flag, most only support a subset of possible options
         // vSeeds.push_back(CDNSSeedData("multidoge.org", "seed.multidoge.org", true));
         // vSeeds.push_back(CDNSSeedData("multidoge.org", "seed2.multidoge.org"));
-=======
+
         for (int (i) = 0; (i) < 100; ++(i)) {
             genesis = CreateGenesisBlock(1681098301, i, 0x207fffff, 1, 12000000 * COIN);
             bool fNegative;
@@ -223,7 +223,7 @@ public:
         consensus.hashGenesisBlock = genesis.GetHash();
         digishieldConsensus.hashGenesisBlock = consensus.hashGenesisBlock;
         auxpowConsensus.hashGenesisBlock = consensus.hashGenesisBlock;
->>>>>>> 14ee81c3d89ce99db8c58bef35983614ec0ed4ce
+
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,30);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,22);
